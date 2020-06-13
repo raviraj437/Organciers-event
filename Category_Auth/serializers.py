45d680ebf3362e572d8961f605d631f58062fd57
Ravi Raj <45043636+raviraj437@users.noreply.github.com>
2020-06-13 14:25:04 +0530

    Update serializers.py

--- Category_Auth/serializers.py
+++ Category_Auth/serializers.py
@@ -1,4 +1,4 @@
-from . models import  Category, Image, Video
+from . models import  Category, Image, Video, Event, Testmonial, ImageTestmonial
 from .models import *
 from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer, UserSerializer
@@ -22,4 +22,19 @@ class UserCreateSerializer(UserCreateSerializer):
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
+        fields = '__all__'
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class TestmonialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Testmonial
+        fields = '__all__'
+
+class ImageTestmonialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImageTestmonial
         fields = '__all__'
