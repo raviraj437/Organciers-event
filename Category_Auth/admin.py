d5d26ada52ded7a299ffb720fb7f0bdbe6edd77d
Ravi Raj <45043636+raviraj437@users.noreply.github.com>
2020-06-13 14:26:36 +0530

    Update admin.py

--- Category_Auth/admin.py
+++ Category_Auth/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Category
 from .models import Image
 from .models import Video
+from .models import Event
+from .models import Testmonial
+from .models import ImageTestmonial
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Image)
 admin.site.register(Video)
+admin.site.register(Event)
+admin.site.register(Testmonial)
+admin.site.register(ImageTestmonial)
